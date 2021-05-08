5c1cc7ba63f669837aa563c56b7f61305a61c1d8
Djuns <q.breneol@gmail.com>
2021-05-08 11:34:55 +0200

    New forlder

---
+++ Chess/models/Players.py
@@ -0,0 +1,12 @@
+### Docstring ###
+
+class Players:
+
+### Docstring ###
+ 
+    def __init__(self):
+        self.last_name = None
+        self.first_name = None
+        self.birth_date = None
+        self.gender = None
+        self.ranking = None
---
+++ Chess/models/Rounds.py
@@ -0,0 +1,12 @@
+### Docstring ###
+
+class Rounds:
+
+### Docstring ###
+
+    def __init__(self):
+        self.name = None
+        self.start_date = None
+        self.start_hour = None
+        self.end_date = None
+        self.end_hour = None
